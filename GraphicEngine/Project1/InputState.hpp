b27dbfec6f576d739e9f84864a1502c353b62a15
Jacek Jarosz <jacekjarosz55@gmail.com>
2024-11-06 22:20:09 +0100

    aflaksdjf

--- GraphicEngine/Project1/InputState.hpp
+++ GraphicEngine/Project1/InputState.hpp
@@ -1,6 +1,10 @@
+#include "Vector2D.hpp"
 #include <SFML/Window/Keyboard.hpp>
 #include <unordered_map>
 class InputState {
-  std::unordered_map<sf::Keyboard::Key, bool> _pressedKeys;
+public:
   bool isKeyPressed(sf::Keyboard::Key key);
+private:
+  std::unordered_map<sf::Keyboard::Key, bool> _pressedKeys;
+  Vector2D _mousePos;
 };
---
+++ GraphicEngine/Project1/UpdatableObject.hpp
@@ -0,0 +1,9 @@
+#ifndef UPDATABLEOBJECT_HPP
+#define UPDATABLEOBJECT_HPP
+#include "Vector2D.hpp"
+class UpdatableObject {
+public:
+  void update(float deltaTime, Input input) {
+  }
+};
+#endif
